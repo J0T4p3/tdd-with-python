eea4760b4905ed94a0ebb7cd17fb89b5d9bd5f03
João Pedro Silva <joaopedrosilvadelima@gmail.com>
2026-01-04 20:14:00 -0300

    Unified tests for / home, ajusted urls at settings.py

--- lists/tests.py
+++ lists/tests.py
@@ -7,17 +7,12 @@ from .views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
     def test_home_page_that_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-
+        response = self.client.get("/")
         html = response.content.decode("utf8")
         self.assertIn("<title>To-do lists</title>", html)
         self.assertTrue(html.startswith("<html>"))
         self.assertTrue(html.endswith("</html>"))
 
-    def test_home_page_that_returns_correct_html_2(self):
-        response = self.client.get("/")
-        self.assertContains(response, "<title>To-do lists</title>")
 
 
 
