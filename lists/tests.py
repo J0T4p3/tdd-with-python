d358a18b1b9340e0910c86d15fc42a051366445e
João Pedro Silva <joaopedrosilvadelima@gmail.com>
2026-01-05 23:38:32 -0300

    fix: removal of unused code

--- lists/tests.py
+++ lists/tests.py
@@ -1,19 +1,12 @@
-from django.http import HttpRequest
 from django.test import TestCase
 
-from .views import home_page
 
-
-# Create your tests here.
 class HomePageTest(TestCase):
     def test_home_page_that_returns_correct_html(self):
+        # By the context, get the view associated with the url
         response = self.client.get("/")
         html = response.content.decode("utf8")
         self.assertIn("<title>To-do lists</title>", html)
         self.assertTrue(html.startswith("<html>"))
         self.assertTrue(html.endswith("</html>"))
-
-
-
-
 
