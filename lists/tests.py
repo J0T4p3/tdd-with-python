c9672cb0b12f8b55354ab990f669f4337d58e595
João Pedro Silva <joaopedrosilvadelima@gmail.com>
2026-01-05 23:54:47 -0300

    fix: removed whitespaces from string in tests

--- lists/tests.py
+++ lists/tests.py
@@ -5,7 +5,7 @@ class HomePageTest(TestCase):
     def test_home_page_that_returns_correct_html(self):
         # By the context, get the view associated with the url
         response = self.client.get("/")
-        html = response.content.decode("utf8")
+        html = response.content.decode("utf8").strip()
         self.assertIn("<title>To-do lists</title>", html)
         self.assertTrue(html.startswith("<html>"))
         self.assertTrue(html.endswith("</html>"))
