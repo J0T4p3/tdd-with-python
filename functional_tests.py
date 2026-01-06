dede54ed440097a634581e0885666ed49fa3044e
João Pedro Silva <joaopedrosilvadelima@gmail.com>
2026-01-05 23:37:06 -0300

    feature: functional tests now validate user todo input

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,8 @@
+import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class NewVisitorsTest(unittest.TestCase):
@@ -17,14 +19,25 @@ class NewVisitorsTest(unittest.TestCase):
 
         # She notices something related with To-do on the title
         self.assertIn("To-do", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("To-do", header_text)
 
         # She are prompted to write a todo right away
-        self.fail('Finish the test')
+        input_box = self.browser.find_element(By.ID, "id_input_todo")
+        self.assertEqual(input_box.get_attribute("placeholder"), "Enter a to-do item")
 
         # She types "make a pasta" on the box and press 'enter'
+        input_box.send_keys("make a pasta")
+        input_box.send_keys(webdriver.Keys.ENTER)
 
         # After she presses the button, a list appear bellow the input box with 1 - make a pasta as it's title
+        time.sleep(1)
+        table = self.browser.find_element(By.ID, "id_todo_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertTrue(any(row.text == "1: make a pasta" for row in rows))
+
         # The box is now empty again, ready to receive new inputs
+        self.fail('Finish the test')
 
         # She writes again, now with the words "Serve the pasta to friends".
         # Again, after pressing 'enter', the words written are now in the second list item,
