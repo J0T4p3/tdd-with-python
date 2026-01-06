f7050cdd79dce6bbac3e66ab345c5857172a89f9
João Pedro Silva <joaopedrosilvadelima@gmail.com>
2026-01-05 23:38:45 -0300

    feature: added more html code for user interface

--- lists/views.py
+++ lists/views.py
@@ -4,4 +4,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse(content="<html><title>To-do lists</title></html>")
+    html = """
+        <html>
+            <title>To-do lists</title>
+            <body>
+                <h1>To-do</h1>
+                <input id="id_input_todo" placeholder="Enter a to-do item"></input>
+                <table id="id_todo_table">
+                    <tr>1: make a pasta</tr>
+                </table>
+            </body>
+        </html>
+    """
+    return HttpResponse(content=html)
