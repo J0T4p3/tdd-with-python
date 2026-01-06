0c72ff75c0c07b7ef256a8ce54bdb3d75abe39e7
João Pedro Silva <joaopedrosilvadelima@gmail.com>
2026-01-05 23:53:20 -0300

    feat: refactored the views to render the page via templates

--- lists/views.py
+++ lists/views.py
@@ -1,19 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 def home_page(request):
-    html = """
-        <html>
-            <title>To-do lists</title>
-            <body>
-                <h1>To-do</h1>
-                <input id="id_input_todo" placeholder="Enter a to-do item"></input>
-                <table id="id_todo_table">
-                    <tr>1: make a pasta</tr>
-                </table>
-            </body>
-        </html>
-    """
-    return HttpResponse(content=html)
+    return render(request, "lists/home_page.html")
