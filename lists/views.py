9f32e391934a52d6beedd14f4b8a6f8189ccaa62
João Pedro Silva <joaopedrosilvadelima@gmail.com>
2026-01-08 23:29:29 -0300

    refactor: using create to store new items at db

--- lists/views.py
+++ lists/views.py
@@ -7,8 +7,7 @@ from .models import Item
 # Create your views here.
 def home_page(request: HttpRequest):
     if request.method == 'POST':
-        todo_item = Item(text=request.POST['todo_item'])
-        todo_item.save()
+        Item.objects.create(text=request.POST['todo_item'])
         return redirect('/')
     return render(request, "lists/home_page.html", {"todo_items": Item.objects.all()})
 
