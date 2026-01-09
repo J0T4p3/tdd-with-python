51437b3880a520717662347213fcec68b245bd52
João Pedro Silva <joaopedrosilvadelima@gmail.com>
2026-01-08 22:50:47 -0300

    feat: Included database storage and presentation of stored items

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from .models import Item
+
 
 # Create your views here.
 def home_page(request: HttpRequest):
-
-    return render(request, "lists/home_page.html", {"todo_item": request.POST.get("todo_item")})
+    todo_item = Item()
+    if todo := request.POST.get("todo_item"):
+        todo_item.text = todo
+        todo_item.save()
+    return render(request, "lists/home_page.html", {"todo_items": Item.objects.all()})
 
